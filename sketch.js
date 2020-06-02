a28c749046074e4eb0ab66b75a6198608f51b329
srita-2007 <65296519+srita-2007@users.noreply.github.com>
2020-06-02 05:00:56 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12, line13, line13, line14, line15, line16,line17,line18,line19,line20,line21,line22,line23,line24,line25,line26,line27,line28,line29,line30
+
+function setup() {
+  var canvas = createCanvas(800,400);
+  var line1 = createSprite(400, 200, 1, 8);
+  var line2 = createSprite(130, 50, 1, 9);
+  var line3 = createSprite(2, 40, 2, 6);
+  var line4 = createSprite(30, 100, 3, 7);
+  var line5 = createSprite(20, 300, 2, 10);
+  var line6 = createSprite(200, 370, 1, 4);
+  var line7 = createSprite(12, 190, 2, 7);
+  var line8 = createSprite(36, 80, 2, 7);
+  var line9 = createSprite(70, 140, 1, 10);
+  var line10 = createSprite(186, 250, 3, 9);
+  var line11 = createSprite(112, 370, 1, 3);
+  var line12 = createSprite(91, 91, 2, 8);
+  var line13 = createSprite(220, 21, 3, 9);
+  var line14 = createSprite(246, 85, 1, 6);
+  var line15 = createSprite(350, 129, 3, 10);
+  var line16 = createSprite(46,51,4 ,20);
+  var line17 = createSprite(77,95 ,5 ,15);
+  var line18 = createSprite(154,260 ,5 ,8);
+  var line19 = createSprite(281,390 ,6 ,19);
+  var line20 = createSprite(242,350 ,4 ,9);
+  var line21 = createSprite(215,311 ,8 ,20);
+  var line22 = createSprite(5,10 ,6 ,18);
+  var line23 = createSprite(303,210 ,5 ,30);
+  var line24 = createSprite(189,258 ,7 ,12 );
+  var line25 = createSprite(190,270 ,4 ,1 );
+  var line26 = createSprite(380,312 ,5 ,10 );
+  var line27 = createSprite(100,380 ,10 ,25 );
+  var line28 = createSprite(257,164 ,3 ,9 );
+  var line29 = createSprite(381,192 ,3 , 27 );
+  var line30 = createSprite(180,241 ,4 ,16 );
+  
+  
+  
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
